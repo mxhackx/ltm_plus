5a5ef50df70a017cdcc05afeeeea6b841ab39cab
mxhackx <mxhackx@gmail.com>
2026-08-09 21:39:25 +0100

    refactoring

--- src/components/card_catalog.tsx
+++ src/components/card_catalog.tsx
@@ -10,12 +10,10 @@ interface Catalog {
     price: number,
     description: string,
     barprice: number,
-    index: number,
     devise?: string,
     width?: number,
-    onOrder: FunctionConstructor,
 };
-export default function CatalogCard({img, price, description, barprice, onOrder, index, devise = "FCFA", width = 180} : Catalog) {
+export default function CatalogCard({img, price, description, barprice, devise = "FCFA", width = 180} : Catalog) {
     return (
     <div className="rounded-2xl p-6 flex flex-col bg-(--orange) border-2">
         <Image src={img} alt={description} width={width} className="bg-white rounded-2xl mb-10"/>
