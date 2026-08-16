653680e2d023039678db6d6f8cb3de8ebd2fdbec
mxhackx <mxhackx@gmail.com>
2026-08-16 04:29:13 +0100

    correction 3

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,7 +6,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../src/generated/prisma/client";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.STORAGE_DATABASE_URL!,
 });
 
 const prisma = new PrismaClient({
